624662221917c5dc27842688be1ade7126647654
PsihovMaster <psihovtest@gmail.com>
2018-02-26 17:19:24 +0000

    css

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// alert("Hello Word!");
+// console.log("Hello JS!");
+
+// let productTitle="Opel Frontera";
+// let productPrice=20000;
+// let isAvailable="true";
+// document.write(productTitle);
+// document.write("<br>");
+// document.write(productPrice);
+// document.write("<br>");
+// document.write(isAvailable);
+// document.write("<br>");
+// document.write("new price");
+// document.write("<br>");
+// productPrice=productPrice*0.9;
+// document.write(productPrice);
+
+const sum = add(10,20,30);
+console.log(sum);
+
+function add(a,b,c) {
+return a+b+c;
+};
